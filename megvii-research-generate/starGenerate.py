7b661a4a6f6f9c7c413265bea892b8d76f6e7b6a
hitcslj <15576782674@163.com>
2022-07-29 15:17:33 +0800

    add some

---
+++ megvii-research-generate/starGenerate.py
@@ -0,0 +1,27 @@
+'''
+input : ECCV2022-RIFE
+output :<a href="https://github.com/megvii-research/ECCV2022-RIFE"><img src="https://img.shields.io/github/stars/megvii-research/ECCV2022-RIFE?label=star&color=red" /></a>
+'''
+import os
+
+res = []
+
+left = '<a href="https://github.com/megvii-research/'
+
+mid = '"><img src="https://img.shields.io/github/stars/megvii-research/'
+
+right = '?label=star&color=red" /></a>'
+
+
+with open('./nameList.txt', 'r') as f:
+     lines = f.readlines()
+     for name in lines:
+        name = name.strip()
+        if len(name)==0:break
+        s = left+name+mid+name+right
+        print(s)
+        res.append(s)
+print(len(res))
+with open('./star.txt', 'w') as f:
+    for line in res:
+        f.write(line+'\n')
